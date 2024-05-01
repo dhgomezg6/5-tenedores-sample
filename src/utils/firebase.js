2d34ef3f86a61dcd389596d06ee5dbdfa7242b00
Daniel Gomez <2gomex@gmail.com>
2024-04-30 21:28:11 -0500

    Actualizacion de firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { apiKey } from './firebaseApiKey';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "apiKey",
+  apiKey: apiKey,
   authDomain: "tenedores-fe96d.firebaseapp.com",
   projectId: "tenedores-fe96d",
   storageBucket: "tenedores-fe96d.appspot.com",
@@ -14,5 +14,14 @@ const firebaseConfig = {
   appId: "1:343081523367:web:211299ba566c151e0c6382"
 };
 
+let firebaseApp;
+console.log(apiKey);
+
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApps()[0]; // Si ya está inicializada, usa esa instancia
+}
+
 // Initialize Firebase
-export const initFirebase = initializeApp(firebaseConfig);
+export const initFirebase = firebaseApp;
