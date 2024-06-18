d2de3de0f446b5a1ead4459de1542ed08fbe2296
Daniel Gomez <2gomex@gmail.com>
2024-06-17 21:25:41 -0500

    Avatar change and select

--- src/components/Account/InfoUser/InfoUser.js
+++ src/components/Account/InfoUser/InfoUser.js
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View } from 'react-native'
 import { Avatar, Text } from "react-native-elements"
-import { getAuth } from "firebase/auth"
+import { getAuth, updateProfile } from "firebase/auth"
+import * as ImagePicker from "expo-image-picker"
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { styles } from "./InfoUser.styles"
 
-export function InfoUser() {
+export function InfoUser(props) {
+  const {setLoading, setLoadingText} = props;
   const {uid, photoUrl, displayName, email} = getAuth().currentUser;
+  const [avatar, setAvatar] = useState(photoUrl)
   
-  const changeAvatar = () => {
-    console.log('Cambiar avatar')
+  const changeAvatar = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4,3],
+    });
+
+    if(!result.canceled) uploadImage(result.assets[0].uri)
+  };
+
+  const uploadImage = async (uri) => {
+    setLoadingText("Actualizando Avatar");
+    setLoading(true);
+
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    const storage = getStorage();
+    const storageRef = ref(storage, `avatar/${uid}`);
+
+    uploadBytes(storageRef, blob).then((snapshot) => {
+      updatePhotoUrl(snapshot.metadata.fullPath)
+    }).catch((error => {
+      console.log(error)
+    }))
+  }
+
+  const updatePhotoUrl = async (imagePath) => {
+    const storage = getStorage();
+    const imageRef = ref(storage, imagePath);
+
+    const imageUrl = await getDownloadURL(imageRef);
+
+    const auth = getAuth();
+    updateProfile(auth.currentUser, {photoURL: imageUrl});
+
+    setAvatar(imageUrl);
+    setLoading(false);
   }
 
   return (
@@ -18,7 +58,7 @@ export function InfoUser() {
         rounded 
         icon={{type: "material", name:"person"}}
         containerStyle={styles.avatar}
-        source={{ uri: photoUrl }}
+        source={{ uri: avatar }}
       >
         <Avatar.Accessory size={24} onPress={changeAvatar} />
       </Avatar>
--- src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
+++ src/screens/Account/UserLoggedScreen/UserLoggedScreen.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View } from 'react-native'
 import { Button } from 'react-native-elements'
 import { InfoUser } from "../../../components/Account"
+import { LoadingModal } from "./../../../components/Shared"
 import { styles } from "./UserLoggedScreen.styles"
 import { getAuth, signOut } from "firebase/auth"
 
 export function UserLoggedScreen() {
+
+  const [loading, setLoading] = useState(false)
+  const [loadingText, setLoadingText] = useState("")
 
   const logout = async () => {
     const auth = getAuth();
@@ -14,13 +18,15 @@ export function UserLoggedScreen() {
 
   return (
     <View>
-      <InfoUser />
+      <InfoUser setLoading={setLoading} setLoadingText={setLoadingText} />
       <Button 
         title="Cerrar sesion" 
         buttonStyle={styles.btnStyles} 
         titleStyle={styles.btnTextStyle}
         onPress={logout}
       ></Button>
+
+      <LoadingModal show={loading} text={loadingText} />
     </View>
   )
 }
