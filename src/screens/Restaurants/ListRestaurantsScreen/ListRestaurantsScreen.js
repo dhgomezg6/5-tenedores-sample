9eea4486252737ef23ae2d0e457f27b73bfeda9e
Daniel Gomez <2gomex@gmail.com>
2024-07-02 20:51:04 -0500

    Ajuste en la carga de los restaurantes

--- src/screens/Restaurants/ListRestaurantsScreen/ListRestaurantsScreen.js
+++ src/screens/Restaurants/ListRestaurantsScreen/ListRestaurantsScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
+import { useIsFocused } from "@react-navigation/native";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { LoadingModal } from "../../../components/Shared";
@@ -12,6 +13,9 @@ export function ListRestaurantsScreen(props) {
   const { navigation } = props;
   const [currentUser, setCurrentUser] = useState(null);
   const [restaurants, setRestaurants] = useState(null);
+  const [error, setError] = useState(null);
+  const [loadingRestaurants, setLoadingRestaurants] = useState(false);
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     const auth = getAuth();
@@ -21,12 +25,19 @@ export function ListRestaurantsScreen(props) {
   }, []);
 
   useEffect(() => {
-    const q = query(collection(db, "restaurans"), orderBy("createdAt", "desc"));
-
-    onSnapshot(q, (snapshot) => {
-      setRestaurants(snapshot.docs);
-    });
-  }, []);
+    if(currentUser){
+      const q = query(collection(db, "restaurans"), orderBy("createdAt", "desc"));
+      setLoadingRestaurants(true);
+      onSnapshot(q, (snapshot) => {
+        setLoadingRestaurants(false);
+        setRestaurants(snapshot.docs);
+      }, (error) => {
+        console.log(error);
+      });
+    } else {
+      setRestaurants(null);
+    }
+  }, [props, isFocused]);
 
   const goToAddRestaurant = () => {
     navigation.navigate(screen.restaurant.tab, {
@@ -36,8 +47,8 @@ export function ListRestaurantsScreen(props) {
 
   return (
     <View style={styles.content}>
-      <Text>Estamos en la screen RestaurantsScreen</Text>
-      {!restaurants ? 
+      <Text>Estamos en la screen RestaurantsScreen2</Text>
+      {loadingRestaurants ? 
         <LoadingModal text="Cargando" /> 
         : <ListRestaurant restaurants={restaurants} />}
 
